968be990bf1650d7b78e697cafcc732bd6b7c24a
andreea <iloaiaandreea@gmail.com>
2020-11-16 15:19:42 +0200

    auth part of the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -22,15 +21,18 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import {AuthProvider, Login} from "./auth";
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
+      <IonReactRouter>
+          <IonRouterOutlet>
+              <AuthProvider>
+                  <Route path="/login" component={Login} exact={true}/>
+                  <Route exact path="/" render={() => <Redirect to="/"/>}/>
+              </AuthProvider>
+          </IonRouterOutlet>
+      </IonReactRouter>
   </IonApp>
 );
 
